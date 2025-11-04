727ed4a4a21ec5e07eca18001e01ede6aefbe433
Alex Silva <alex.paulo100@gmail.com>
2025-11-04 11:02:28 -0300

    add

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 # state.tf
 terraform {
   backend "s3" {
-    bucket  = "terraform-state"
+    bucket  = "terraform-state-alexsilva-us-east-2"
     key     = "site/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
